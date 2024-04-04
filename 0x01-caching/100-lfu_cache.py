4ca0a2918b46d9260123c50243a1b1d8179da267
elbatouri rabi <elbatouri@gmail.com>
2024-04-04 00:54:59 +0000

    push correction task 100 advanced'

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 
 from base_caching import BaseCaching
 
+
 class LFUCache(BaseCaching):
     """Represents an object that allows storing and
     retrieving items from a dictionary with an LFU
