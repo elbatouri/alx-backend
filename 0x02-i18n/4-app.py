123ab1f91f3b6177ceedc3acfc06cd44891faf1e
elbatouri rabi <elbatouri@gmail.com>
2024-04-09 23:01:06 +0000

    correct task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-"""A Flask app supporting internationalization."""
+"""A Basic Flask app with internationalization support.
+"""
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
 class Config:
-    """Configuration for Flask Babel."""
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -19,7 +21,8 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieve the locale for a web page."""
+    """Retrieves the locale for a web page.
+    """
     queries = request.query_string.decode('utf-8').split('&')
     query_table = dict(map(
         lambda x: (x if '=' in x else '{}='.format(x)).split('='),
@@ -33,7 +36,8 @@ def get_locale() -> str:
 
 @app.route('/')
 def get_index() -> str:
-    """Display the home/index page."""
+    """The home/index page.
+    """
     return render_template('4-index.html')
 
 
