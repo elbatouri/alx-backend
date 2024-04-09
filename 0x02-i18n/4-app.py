72e16bb4010f1440d59b3e4e59f0d39da3b27ec8
elbatouri rabi <elbatouri@gmail.com>
2024-04-09 23:26:16 +0000

    correct tasks

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python3
-"""A Basic Flask app with internationalization support.
 """
-from flask_babel import Babel
+Force locale with URL parameter
+"""
+
+import babel
 from flask import Flask, render_template, request
-
-
-class Config:
-    """Represents a Flask Babel configuration.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
+from flask_babel import Babel
 
 app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
+class Config:
     """
-    queries = request.query_string.decode('utf-8').split('&')
-    query_table = dict(map(
-        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
-        queries,
-    ))
-    if 'locale' in query_table:
-        if query_table['locale'] in app.config["LANGUAGES"]:
-            return query_table['locale']
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-@app.route('/')
-def get_index() -> str:
-    """The home/index page.
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    detect if the incoming request contains locale
+    argument and ifs value is a supported locale, return it
+    """
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    hello world
     """
     return render_template('4-index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
